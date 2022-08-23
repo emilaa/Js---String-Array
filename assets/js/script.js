71e5822097fd412d187daf06b78f975f092c50f6
emilaa <emilaa@code.edu.az>
2022-08-23 19:19:34 +0400

    Homework

---
+++ assets/js/script.js
@@ -0,0 +1,150 @@
+// let text =
+//   "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+
+//   let result = text.substring(0,50) + " ...";
+
+//   console.log(result);
+
+// let num = 66;
+
+// console.log(num.toString());
+// console.log(country.length);
+
+// for (const iterator of country) {
+//   console.log(iterator);
+// }
+
+// console.log(country[1]);
+
+// console.log(country.toLocaleUpperCase());
+
+// console.log(country.slice(4));
+
+// console.log(country.substring(0,7));
+
+// let country = "Azerbaycan";
+// let addStr = " Respublikasi"
+
+// console.log(country.substring(1,5));
+
+// console.log(country.replace("bay", "Salam"));
+
+// console.log(country.concat(addStr));
+
+// console.log(addStr.trim());
+
+// console.log(country.startsWith("A"));
+
+// if (country.startsWith("A")) {
+//     console.log("Yes");
+// }else{
+//     console.log("No");
+// }
+
+// console.log(country.charAt(3));
+
+// console.log(country.charCodeAt(0));
+
+// let result = country.split("-");
+// console.log(result);
+
+// let country = "Azerbaycan-Respublikasi-Mudafie-Azerbaycan-Nazirliyi-Azerbaycan";
+
+// for (let i = 0; i < country.length; i++) {
+//     console.log(country[i]);
+// }
+
+// console.log(country.indexOf("Azerbaycan"));
+
+// function search(str){
+//     if (country.indexOf(str) == -1) {
+//         console.log("Not found");
+//         return;
+//     }
+//     console.log("Found");
+// }
+
+// search("test");
+
+//let country = "Azerbaycan-Respublikasi-Mudafie-Azerbaycan-Nazirliyi-Azerbaycan";
+
+// let firstIndex = country.indexOf("Azerbaycan", 15);
+// let countryIndex = country.indexOf("Azerbaycan", firstIndex + 1);
+// let result = country.substring(0, countryIndex) + " ...";
+
+// console.log(countryIndex);
+// console.log(result);
+
+let city = "Siyezen";
+
+// console.log(city.includes("y"));
+
+// console.log(Number(true));
+// console.log(Number(false));
+// console.log(Number("10"));
+// console.log(Number("Jhon"));
+
+// Number(true);
+// Number(false);
+// Number("10");
+// Number("  10");
+// Number("10  ");
+// Number(" 10  ");
+// Number("10.33");
+// Number("10.33");
+// Number("10.33");
+// Number("Jhon");
+
+// let count = "nfrjnfdff";
+
+// console.log(typeof count);
+
+// let result = int.parse(count);
+
+// console.log(typeof result);
+
+// let users = ["Emil", "Saadat", "Fidan", "Orxan", "Said"];
+
+// console.log(users.toString());
+
+// console.log(users.join(" "));
+
+// users.push("Cavidan")
+
+// users.pop();
+
+// users.unshift("salam");
+
+// users.unshift(2);
+
+// console.log(users);
+
+// const myGirls = ["Saadat", "Fidan"];
+// const myBoys = ["Emil", "Orxan", "Said"]
+
+// const myChildren = myGirls.concat(myBoys);
+
+// console.log(myChildren);
+
+// users.splice(3,2,"Cavidan");
+
+// let users = ["Emil", "Saadat", "Fidan", "Orxan", "Said"];
+
+// console.log(users);
+
+// let result = users.slice(2,2,"Togrul");
+
+// console.log(users);
+
+// const fruits = ["Banana", "Orange", "Lemon", "Apple", "Mango"];
+// const citrus = fruits.slice(1, 3);
+
+// console.log(citrus);
+
+// users.sort();
+// console.log(users);
+
+// const points = [40, 100, 1, 5, 25, 10];
+// console.log(points);
+// points.sort(function(a, b){return a - b});
+// console.log(points);
